1d3c7e0602286f73034027e90074c472ba695779
Xavier Olland <xavier.olland@gmail.com>
2015-08-27 16:26:53 +0300

    Small fix
    Copy pasting is bad

--- integration/simplejs.js
+++ integration/simplejs.js
@@ -9,13 +9,15 @@ Get your Slack API token and channel ID there : https://api.slack.com/web
 
 //Set support status to 0 -> no support available
 var supportStatus = 0;
-
+//Replace token and channel ID here
+var token = 'Slack API token';
+var channel = 'Slack channel ID';
 //Get channel information
-var channel = function (token, channel) {
+var channelInfo = function () {
 	$.getJSON("https://slack.com/api/channels.info?token="+token+"&channel="+channel+"&pretty=1", members(data,token));
 }
 //Check members online status
-var members = function (data, token) {
+var members = function (data) {
 	var members = data.channel.members;
 	for (var i in members) {
 		$.getJSON("https://slack.com/api/users.getPresence?token="+token+"&user="+members[i]+"&pretty=1", presence);
@@ -32,5 +34,4 @@ var presence = function(data){
   if(supportStatus > 0){ SupportKit.init({appToken: 'your app token here'}); }
 };
 
-//Replace token and channel ID here
-$(document).ready( channel('your slack API token here','your channel ID here') );
+$(document).ready( channelInfo );
